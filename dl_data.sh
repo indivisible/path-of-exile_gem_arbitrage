0b71eeab1006a9bd1c5d2553a61187d489ee54e9
Zoltán <islandofcalmness@gmail.com>
2021-11-12 17:59:42 +0100

    create data dir if needed

--- dl_data.sh
+++ dl_data.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+[ -d "data" ] || mkdir "data"
+
 wget -O "data/poedb_quality.html" "https://poedb.tw/us/Quality"
 wget -O "data/gem_prices.json" "https://poe.ninja/api/data/itemoverview?league=Scourge&type=SkillGem"
 wget -O "data/currency.json" "https://poe.ninja/api/data/CurrencyOverview?league=Scourge&type=Currency&language=en"
