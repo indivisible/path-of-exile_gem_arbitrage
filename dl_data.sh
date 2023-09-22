fbf772fe085754ca3a611319edf2b21647d4fb34
Zoltán <islandofcalmness@gmail.com>
2023-09-22 10:52:43 +0200

    add variable for specifying league

--- dl_data.sh
+++ dl_data.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
 
+LEAGUE="Ancestor"
+
 set -e
 
 [ -d "data" ] || mkdir "data"
 
 wget -O "data/poedb_quality.html" "https://poedb.tw/us/Quality"
-wget -O "data/gem_prices.json" "https://poe.ninja/api/data/itemoverview?league=Scourge&type=SkillGem"
-wget -O "data/currency.json" "https://poe.ninja/api/data/CurrencyOverview?league=Scourge&type=Currency&language=en"
+wget -O "data/gem_prices.json" "https://poe.ninja/api/data/itemoverview?league=$LEAGUE&type=SkillGem"
+wget -O "data/currency.json" "https://poe.ninja/api/data/CurrencyOverview?league=$LEAGUE&type=Currency&language=en"
